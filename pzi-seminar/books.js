f133a2d6f52bd10650740d3d7c665eacc77b0e9f
nbilic <nbilic00@fesb.hr>
2024-02-23 21:32:35 +0100

    stuff

---
+++ pzi-seminar/books.js
@@ -0,0 +1,103 @@
+const books = [
+  {
+    name: "The Great Gatsby",
+    id: 1,
+    author: "F. Scott Fitzgerald",
+    genre: "Classic",
+    poster:
+      "https://assets.mubicdn.net/images/notebook/post_images/31857/images-w1400.jpg?1607290863",
+    description:
+      "A novel depicting the roaring twenties and the American Dream.",
+  },
+  {
+    name: "To Kill a Mockingbird",
+    id: 2,
+    author: "Harper Lee",
+    genre: "Fiction",
+    poster:
+      "https://assets.mubicdn.net/images/notebook/post_images/31857/images-w1400.jpg?1607290863",
+    description:
+      "A powerful exploration of racial injustice in the deep South.",
+  },
+  {
+    name: "1984",
+    id: 3,
+    author: "George Orwell",
+    genre: "Dystopian",
+    poster:
+      "https://assets.mubicdn.net/images/notebook/post_images/31857/images-w1400.jpg?1607290863",
+    description: "A classic novel depicting a totalitarian future society.",
+  },
+  {
+    name: "The Hobbit",
+    id: 4,
+    author: "J.R.R. Tolkien",
+    poster:
+      "https://assets.mubicdn.net/images/notebook/post_images/31857/images-w1400.jpg?1607290863",
+    genre: "Fantasy",
+    description:
+      "An epic fantasy adventure following the journey of Bilbo Baggins.",
+  },
+  {
+    name: "Pride and Prejudice",
+    id: 5,
+    author: "Jane Austen",
+    genre: "Romance",
+    poster:
+      "https://assets.mubicdn.net/images/notebook/post_images/31857/images-w1400.jpg?1607290863",
+    description:
+      "A classic novel exploring themes of love, class, and social expectations.",
+  },
+  {
+    name: "The Catcher in the Rye",
+    id: 6,
+    author: "J.D. Salinger",
+    genre: "Coming-of-age",
+    poster:
+      "https://assets.mubicdn.net/images/notebook/post_images/31857/images-w1400.jpg?1607290863",
+    description:
+      "A novel narrated by a disenchanted teenager navigating the adult world.",
+  },
+  {
+    name: "The Lord of the Rings",
+    id: 7,
+    author: "J.R.R. Tolkien",
+    genre: "Fantasy",
+    poster:
+      "https://assets.mubicdn.net/images/notebook/post_images/31857/images-w1400.jpg?1607290863",
+    description:
+      "An epic high-fantasy trilogy set in the world of Middle-earth.",
+  },
+  {
+    name: "Harry Potter and the Sorcerer's Stone",
+    id: 8,
+    author: "J.K. Rowling",
+    genre: "Fantasy",
+    poster:
+      "https://assets.mubicdn.net/images/notebook/post_images/31857/images-w1400.jpg?1607290863",
+    description:
+      "The first book in the magical series chronicling the life of Harry Potter.",
+  },
+  {
+    name: "The Alchemist",
+    id: 9,
+    author: "Paulo Coelho",
+    genre: "Philosophical",
+    poster:
+      "https://assets.mubicdn.net/images/notebook/post_images/31857/images-w1400.jpg?1607290863",
+    description:
+      "A novel following the journey of a shepherd named Santiago in search of treasure.",
+  },
+  {
+    name: "The Hunger Games",
+    id: 10,
+    author: "Suzanne Collins",
+    poster:
+      "https://assets.mubicdn.net/images/notebook/post_images/31857/images-w1400.jpg?1607290863",
+    genre: "Dystopian",
+    description:
+      "A dystopian trilogy set in a post-apocalyptic world where teenagers fight for survival.",
+  },
+];
+
+export { books };
--- pzi-seminar/script.js
+++ pzi-seminar/script.js
@@ -1,33 +1,109 @@
-// JavaScript function to show/hide sections based on clicked link
+import { books } from "./books.js";
+
+const library = [];
+
 function showSection(sectionId) {
-    // Hide all sections
-    var sections = document.querySelectorAll('main section');
-    sections.forEach(function (section) {
-        section.classList.add('hidden');
-    });
-    // Show the selected section
-    var selectedSection = document.getElementById(sectionId);
-    selectedSection.classList.remove('hidden');
+  const sections = document.querySelectorAll("main section");
+  sections.forEach(function (section) {
+    section.classList.add("hidden");
+  });
+  const selectedSection = document.getElementById(sectionId);
+  selectedSection.classList.remove("hidden");
 }
 
 function openPopup() {
-    alert("Server Offline");
+  alert("Server Offline");
 }
 
-// JavaScript function to hide label::before when text is input
-document.addEventListener('DOMContentLoaded', function () {
-    var inputs = document.querySelectorAll('input[type="text"], input[type="email"], textarea');
-    inputs.forEach(function (input) {
-        input.addEventListener('input', function () {
-            var label = this.parentNode.querySelector('label');
-            label.classList.toggle('has-text', this.value.trim() !== '');
-        });
+document.addEventListener("DOMContentLoaded", function () {
+  const inputs = document.querySelectorAll(
+    'input[type="text"], input[type="email"], textarea'
+  );
+  inputs.forEach(function (input) {
+    input.addEventListener("input", function () {
+      const label = this.parentNode.querySelector("label");
+      label.classList.toggle("has-text", this.value.trim() !== "");
     });
+  });
 });
 
 window.onload = function () {
-    var inputs = document.querySelectorAll('input[type="text"], input[type="email"], textarea');
-    inputs.forEach(function (input) {
-        input.value = ''; // Set input value to an empty string
-    });
+  const inputs = document.querySelectorAll(
+    'input[type="text"], input[type="email"], textarea'
+  );
+  inputs.forEach(function (input) {
+    input.value = "";
+  });
 };
+
+// New stuff
+
+const createElement = (type) => document.createElement(type);
+const getElementById = (id) => document.getElementById(id);
+const querySelector = (query) => document.querySelector(query);
+
+const generateCategories = () => {
+  const categories = books
+    .map((book) => book.genre)
+    .filter((value, index, self) => {
+      return self.indexOf(value) === index;
+    });
+
+  const element = getElementById("categories-table");
+  categories.forEach((category) => {
+    const li = createElement("li");
+    li.innerHTML = category;
+    li.addEventListener("click", () => filterBooksByGenre(category));
+    element.append(li);
+  });
+};
+
+const addToLibrary = (book) => {
+  if (!library.includes(book)) {
+    library.push(book);
+    alert(`Added ${book.name} to library`);
+  }
+  console.log(library);
+};
+
+const clearBestSellers = () => {
+  const sellers = querySelector(".best-sellers");
+  sellers.innerHTML = "";
+};
+
+const filterBooksByName = () => {
+  const string = getElementById("search-bar").value;
+  const filtered = books.filter((book) => book.name.includes(string));
+  displayBooks(filtered);
+};
+
+const filterBooksByGenre = (genre) => {
+  displayBooks(books.filter((book) => book.genre === genre));
+};
+
+const createBookDiv = (book) => {
+  const div = createElement("div");
+  const img = createElement("img");
+  const p = createElement("p");
+
+  img.src = book.poster;
+  p.innerHTML = book.name;
+
+  div.append(img);
+  div.append(p);
+  div.addEventListener("click", () => addToLibrary(book));
+  return div;
+};
+
+const displayBooks = (books) => {
+  clearBestSellers();
+  const sellers = querySelector(".best-sellers");
+
+  books.forEach((book) => {
+    sellers.append(createBookDiv(book));
+  });
+};
+
+getElementById("search-btn").addEventListener("click", filterBooksByName);
+
+generateCategories();
